d8db8cca420dd2186ab4f5eb6908227bc396e97b
Callum Dawson <cdawson198@gmail.com>
2021-11-23 14:43:40 +1100

    forms and index

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,3 +1,12 @@
 class Car < ApplicationRecord
   belongs_to :user
+  has_many :bookings
+  has_many :bookings_reviews, through: :bookings
+  validates :seats, presence: true
+  validates :price, presence: true
+  validates :make, presence: true
+  validates :model, presence: true
+  validates :transmission, presence: true
+  validates :car_type, presence: true
+  validates :suburb, presence: true
 end
---
+++ app/policies/car_policy.rb
@@ -0,0 +1,29 @@
+class CarPolicy < ApplicationPolicy
+  def new?
+    true
+  end
+
+  def show?
+    true
+  end
+
+  def update?
+    user_is_owner?
+  end
+
+  def create?
+    user_is_owner?
+  end
+
+  class Scope < Scope
+    def resolve
+      scope.where(user: user)
+    end
+  end
+
+  private
+
+  def user_is_owner?
+    @record.user == @user
+  end
+end
