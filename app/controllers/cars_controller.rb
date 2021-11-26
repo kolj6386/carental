f2146b34e443b7a31d07b1be32f7cebac97e9197
sholom sero <sholomsero123@gmail.com>
2021-11-26 12:50:34 +1100

    finished positioning besides bottom alignment

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -3,12 +3,12 @@ class CarsController < ApplicationController
 
   def index
     @cars = policy_scope(Car)
-      @markers = @cars.geocoded.map do |car|
+    @markers = @cars.geocoded.map do |car|
       {
         lat: car.latitude,
         lng: car.longitude
       }
-      end
+    end
   end
 
   def new
